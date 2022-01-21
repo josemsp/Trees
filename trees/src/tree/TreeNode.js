e8d526ac9ea272fea81408a0cef18ff8e3895160
JSalper <jose_salazar_@outlook.com>
2022-01-20 21:25:39 -0600

    Let's add interactions

--- trees/src/tree/TreeNode.js
+++ trees/src/tree/TreeNode.js
@@ -1,22 +1,55 @@
+import { useState } from "react";
+
 const TreeNode = (props) => {
-  const { node, children, dots = '' } = props
-  const name = Array.from(node)
+  const { node, children, dots = '', father, addChildren, deleteChildren, addInput } = props
+  let name = Array.from(node || '')
   name.splice(1, 0, dots)
+  name = name.join('')
+
+  const [text, setText] = useState('')
+
+  const handleText = (event) => setText(event.target.value)
+
+  const handleKeyUp = (event) => {
+    const keycode = event.keyCode;
+    const code = event.code;
+
+    if (text !== '' && (keycode === '13' || code === 'Enter')) {
+      addChildren({ father: node, name: text })
+      setText('')
+    }
+  }
+
+  const handleDelete = () => deleteChildren({ father, node })
+
+  const handleAddInput = () => addInput({ father, node: node })
 
   return (
     <div className='tree-node'>
-      {`${name.join('')}`}
+      {`${name}`}
+
+      {father &&
+        <div className="options">
+          <span role="img" aria-label="cross" onClick={handleDelete}>❌</span>
+          {!children && <span role="img" aria-label="cross" onClick={handleAddInput}>➕</span>}
+        </div>
+      }
+
       {children &&
         <div key={node.node} className="indent">
-          {children.map((node) => (
+          {children.map((_node) => (
             <TreeNode
-              {...node}
+              {..._node}
               dots={`${dots}.`}
               father={node}
+              addChildren={addChildren}
+              deleteChildren={deleteChildren}
+              addInput={addInput}
             />
           ))}
         </div>
       }
+      {children && <input type="text" value={text} onChange={handleText} onKeyUp={handleKeyUp} />}
     </div>
   );
 };
--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,11 +1,79 @@
 import "./index.css";
 import TreeNode from "./TreeNode";
 import dataJSON from './data.json'
+import { useState } from "react";
 
 export default function Tree () {
+  const [data, setData] = useState(dataJSON)
+
+  const copyObject = (obj) => JSON.parse(JSON.stringify(obj))
+
+  const handleAdd = ({ father, name }) => {
+    let _data = copyObject(data)
+    const result = searchInTree(_data, father, name, addChildren)
+    if (JSON.stringify(result) !== JSON.stringify(data)) {
+      setData(result)
+    }
+  }
+
+  const addChildren = (_element, name) => {
+    _element.children = [
+      ..._element.children,
+      { node: name }
+    ]
+    return _element
+  }
+
+  const handleDelete = ({ father, node }) => {
+    let _data = copyObject(data)
+    const result = searchInTree(_data, father, node, deleteChildren)
+    if (JSON.stringify(result) !== JSON.stringify(data)) {
+      setData(result)
+    }
+  }
+
+  const deleteChildren = (_element, name) => {
+    _element.children = _element.children.filter(n => n.node !== name)
+    return _element
+  }
+
+  const addInput = ({ father, node }) => {
+    let _data = copyObject(data)
+    const result = searchInTree(_data, father, node, setChildren)
+    if (JSON.stringify(result) !== JSON.stringify(data)) {
+      setData(result)
+    }
+  }
+
+  const setChildren = (_element, name) => {
+    let _node = _element.children.find(n => n.node === name)
+    _node.children = []
+    return _element
+  }
+
+  const searchInTree = (element, nodeToFind, name, action) => {
+    if (element.node === nodeToFind) {
+      element = { ...action(element, name) }
+      return element
+    } else if (element.children !== undefined) {
+      let index = 0
+      let result = null
+      for (index = 0; result === null && index < element.children.length; index++) {
+        result = searchInTree(element.children[index], nodeToFind, name, action)
+      }
+      if (result) return element
+    }
+    return null
+  }
+
   return (
     <div className="indent">
-      <TreeNode {...dataJSON} />
+      <TreeNode
+        {...data}
+        addChildren={handleAdd}
+        deleteChildren={handleDelete}
+        addInput={addInput}
+      />
     </div>
   );
 }
